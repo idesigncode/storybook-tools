0430b085de8b6b185cee19eee6e25ac8f7c24169
Matthew Nuthall <15828266+idesigncode@users.noreply.github.com>
2023-05-04 15:31:09 +1000

    Removed example code

--- src/exampleFunction.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * TODO - delete once src files setup
- */
-export default (parameter) => {
-  return parameter;
-};
--- stories/ExampleComponentWithProps.mjs
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ExampleComponent from '../src/ExampleComponent.mjs';
-
-const ExampleComponentWithProps = () => {
-  return <ExampleComponent className="ExampleComponentWithProps" />;
-};
-
-export default ExampleComponentWithProps;
