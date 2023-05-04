91cdb8177ec0ee0f499a3ad60c268a1f531d6fec
Matthew Nuthall <15828266+idesigncode@users.noreply.github.com>
2023-05-04 15:57:50 +1000

    Added DefaultValue component

---
+++ src/DefaultValue.mjs
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import formatValueToString from './formatValueToString.mjs';
+import getValueType from './getValueType.mjs';
+import Source from './Source.mjs';
+
+const DefaultValue = ({ link: initialLink, type, value }) => {
+  const valueType = type || getValueType(value);
+
+  let link = initialLink;
+  let valueString = formatValueToString(value, valueType);
+  if (valueType === 'ref object') {
+    link = 'https://react.dev/learn/referencing-values-with-refs';
+    valueString = 'See React docs';
+  }
+
+  return (
+    <div className="defaultValue sb-anchor">
+      <table>
+        <tbody>
+          <tr>
+            <th>Default value:</th>
+            <td>
+              <code>{valueType}</code>
+            </td>
+            <td>
+              {link ? (
+                <a href={link} target="_blank" rel="noreferrer">
+                  {valueString}
+                </a>
+              ) : (
+                <Source code={valueString} />
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+DefaultValue.propTypes = {
+  link: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.any.isRequired,
+};
+
+export default DefaultValue;
