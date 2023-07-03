c5e616725ff9f7115c11f782dda3aa4631936622
Matthew Nuthall <15828266+idesigncode@users.noreply.github.com>
2023-07-03 17:14:24 +1000

    Documented CSS variables set by themes

--- stories/CSS.stories.mjs
+++ stories/CSS.stories.mjs
@@ -1,4 +1,5 @@
 import Source from '../src/Source.mjs';
+import ThemesRaw from '../src/themes.scss?raw';
 
 export default {
   title: 'Configuration/CSS',
@@ -20,7 +21,7 @@ export const SeparateStylesheets = {
       `// .storybook/preview.mjs`,
       ``,
       `// General styles`,
-      `import '@idesigncode/storybook-tools/themes.css';`,
+      `import '@idesigncode/storybook-tools/themes.css'; // See below for specific CSS vars`,
       `import '@idesigncode/storybook-tools/storybook.css';`,
       ``,
       `// Component specific styles`,
@@ -28,5 +29,31 @@ export const SeparateStylesheets = {
       `import '@idesigncode/storybook-tools/PropsTable.css';`,
       `import '@idesigncode/storybook-tools/Source.css';`,
     ].join('\n'),
+  },
+};
+
+export const SeparateImportVars = {
+  args: {
+    code: Array.from(
+      // Use "new Set()" to only use unique variables
+      new Set(
+        `${ThemesRaw}`
+          // Split each line to array
+          .split('\n')
+          // Keep only lines with CSS variables
+          .filter((line) => {
+            return line.trim().startsWith('--');
+          })
+          .map((line) => {
+            // Remove CSS variables values
+            const [cssVarDeclaration] = line.trim().split(':');
+            return cssVarDeclaration;
+          })
+          // Sort alphabetically
+          .sort()
+      )
+    )
+      // Rejoin as multiline string
+      .join('\n'),
   },
 };
