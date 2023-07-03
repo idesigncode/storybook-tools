e391922dd3cefbae32434e984c727633526486ee
Matthew Nuthall <15828266+idesigncode@users.noreply.github.com>
2023-07-03 16:33:01 +1000

    Separate style sheets for optional importation

--- stories/CSS.stories.mjs
+++ stories/CSS.stories.mjs
@@ -13,3 +13,20 @@ export const CombinedStylesheet = {
     ].join('\n'),
   },
 };
+
+export const SeparateStylesheets = {
+  args: {
+    code: [
+      `// .storybook/preview.mjs`,
+      ``,
+      `// General styles`,
+      `import '@idesigncode/storybook-tools/themes.css';`,
+      `import '@idesigncode/storybook-tools/storybook.css';`,
+      ``,
+      `// Component specific styles`,
+      `import '@idesigncode/storybook-tools/DefaultValue.css';`,
+      `import '@idesigncode/storybook-tools/PropsTable.css';`,
+      `import '@idesigncode/storybook-tools/Source.css';`,
+    ].join('\n'),
+  },
+};
